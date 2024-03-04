7dd7207eed27565e01b8ca0ac551fda5da1d6b68
Oleksandr <alexgrimm87@gmail.com>
2024-03-04 20:16:11 +0200

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import Home from "./components/home";
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import LoginPage from "./components/auth/login";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<LoginPage />} />
+      </Routes>
     </div>
   );
 }
