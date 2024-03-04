06179dc391c81ccf1af89bcbe30caa74b81d0c27
Oleksandr <alexgrimm87@gmail.com>
2024-03-04 20:26:23 +0200

    add private route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import Home from "./components/home";
-import './App.css';
 import {Route, Routes} from "react-router-dom";
+import Home from "./components/home";
 import LoginPage from "./components/auth/login";
+import './App.css';
+import PrivateRoute from "./utils/router/privateRoute";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<Home />} />
+        </Route>
         <Route path="login" element={<LoginPage />} />
       </Routes>
     </div>
