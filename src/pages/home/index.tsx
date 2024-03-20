ed7233ce99228d1234f22ccaa4889b038b8f884a
Oleksandr <alexgrimm87@gmail.com>
2024-03-20 17:15:29 +0200

    homepage add line chart

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,28 +3,33 @@ import {Box, Grid, useTheme} from "@mui/material";
 import {tokens} from "../../theme";
 import {useAppDispatch, useAppSelector} from "../../utils/hook";
 import {getFavoriteAssets} from "../../store/thunks/assets";
+import {IChartData, ISingleAsset} from "../../common/types/assets";
 import AreaChart from "../../components/charts/area-chart";
+import LineChart from "../../components/charts/line-chart";
 import TrendUp from '../../assets/images/chart/trend-up.svg';
 import TrendDown from '../../assets/images/chart/trend-down.svg';
 
 const Home: FC = (): JSX.Element => {
-  const favoriteAssets: any[] = useAppSelector(state => state.assets.favoriteAssets);
+  const favoriteAssets: IChartData[] = useAppSelector(state => state.assets.favoriteAssets);
   const dispatch = useAppDispatch();
   const fetchDataRef = useRef(false);
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
   const favoriteAssetName = useMemo(() => ['bitcoin', 'ethereum'], []);
-  const filteredArray = favoriteAssets.filter(
-    (value, index, self) =>
-      index === self.findIndex((t) => t.name === value.name)
-  );
+
+  const filteredArray = useMemo(() => {
+    return favoriteAssets.filter(
+      (value, index, self) =>
+        index === self.findIndex((t) => t.name === value.name)
+    )
+  }, [favoriteAssets]);
 
   const fetchData = useCallback((data: string[]) => {
     data.forEach((element: string) => {
       dispatch(getFavoriteAssets(element));
     });
-  }, [dispatch])
+  }, [dispatch]);
 
   useEffect(() => {
     if (fetchDataRef.current) return
@@ -32,10 +37,14 @@ const Home: FC = (): JSX.Element => {
     fetchData(favoriteAssetName);
   }, [favoriteAssetName, fetchData]);
 
-  const renderFavoriteBlock = filteredArray.map((element: any) => {
-    const currentPrice = element.singleAsset.map((element: any) => element.current_price);
-    const currentCap = element.singleAsset.map((element: any) => element.market_cap);
-    const changePrice = element.singleAsset.map((element: any) => element.price_change_percentage_24h);
+  const renderFavoriteBlock = filteredArray.map((element: IChartData) => {
+    let currentPrice = 0;
+    let changePrice = 0;
+
+    element.singleAsset.forEach((element: ISingleAsset) => {
+      currentPrice = element.current_price
+      changePrice = element.price_change_percentage_24h
+    });
 
     return (
       <Grid item xs={12} sm={6} lg={6} key={element.name}>
@@ -74,9 +83,7 @@ const Home: FC = (): JSX.Element => {
               <Box
                 sx={[
                   {
-                    // width: '20%',
-                    // display: 'flex',
-                    // alignItems: 'center',
+                    display: 'flex',
                     alignSelf: 'flex-start',
                     padding: '2px',
                     borderRadius: '4px'
@@ -102,7 +109,7 @@ const Home: FC = (): JSX.Element => {
             </div>
           </Grid>
           <Grid item xs={12} sm={6} lg={6}>
-            <AreaChart data={element.data} />
+            <AreaChart data={element.price_chart_data} />
           </Grid>
         </Grid>
       </Grid>
@@ -111,8 +118,23 @@ const Home: FC = (): JSX.Element => {
 
   return (
     <Box sx={{flexGrow: 1, padding: '32px'}}>
-      <Grid container spacing={2}>
+      <Grid container spacing={2} sx={{marginBottom: '32px'}}>
         {renderFavoriteBlock}
+      </Grid>
+      <Grid container sx={{
+        backgroundColor: `${
+          theme.palette.mode === 'light'
+            ? colors.primary.DEFAULT
+            : colors.primary[600]
+        }`,
+        padding: '20px 16px',
+        minHeight: 270,
+        border: `1px solid ${colors.borderColor}`,
+        borderRadius: '12px'
+      }}>
+        <Grid item xs={12} sm={12} lg={12}>
+          {filteredArray.length && <LineChart data={filteredArray} />}
+        </Grid>
       </Grid>
     </Box>
   );
